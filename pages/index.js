768f0a8965c8b0db7f17913533822214ee6fa7d8
Willian Gonçalves <willian.p.g@outlook.com>
2026-03-04 14:22:44 -0300

    adiciona scripts`lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-  return (<h1>Andrieli, te amo!</h1>)
+function Home() {
+  return <h1>Andrieli, te amo!</h1>;
+}
+
+function Home() {
+  return <h1>Andrieli, te amo!</h1>;
 }
 
 export default Home;
